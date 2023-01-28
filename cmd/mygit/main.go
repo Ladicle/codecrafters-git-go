77918d6fa4f03fa4c6f51f7a2c8b91af186f79f7
Aya Igarashi <ladiclexxx@gmail.com>
2023-01-28 09:58:58 +0900

    Add debug command

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -12,7 +12,12 @@ import (
 	"path/filepath"
 )
 
-const separator = "\x00"
+const SepNull = "\x00"
+
+const (
+	TypeTree = "40000"
+	TypeBlob = "100644"
+)
 
 // Usage: your_git.sh <command> <arg1> <arg2> ...
 func main() {
@@ -41,8 +46,40 @@ func run() error {
 	case "ls-tree":
 		// ./your_git.sh ls-tree --name-only <tree_sha>
 		return runLsTreeCmd(os.Args[3])
+	case "debug":
+		// your_git.sh debug <hash>
+		return runDebugCmd(os.Args[2])
 	}
 	return fmt.Errorf("unknown command: %s", cmd)
+}
+
+func runDebugCmd(hash string) error {
+	data, err := decodeObject(hash)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(len(data))
+
+	// header
+	buf := bytes.NewBuffer(data)
+	header, _ := buf.ReadString('\000')
+	header = header[:len(header)-1]
+	fmt.Printf("%s\n", header)
+
+	// tree entries
+	for {
+		mode, err := buf.ReadString(' ')
+		if err != nil {
+			break
+		}
+		mode = mode[:len(mode)-1]
+		name, _ := buf.ReadString('\000')
+		name = name[:len(name)-1]
+		sha := buf.Next(20)
+		fmt.Printf("%v %s %x\n", tpeToStr(mode), name, sha)
+	}
+	return nil
 }
 
 func runInitCmd() error {
@@ -62,24 +99,11 @@ func runInitCmd() error {
 }
 
 func runCatFileCmd(sha string) error {
-	dirName := sha[:2]
-	fileName := sha[2:]
-
-	f, err := os.Open(filepath.Join(".git", "objects", dirName, fileName))
+	data, err := decodeObject(sha)
 	if err != nil {
 		return err
 	}
-
-	r, err := zlib.NewReader(f)
-	if err != nil {
-		return err
-	}
-
-	data, err := ioutil.ReadAll(r)
-	if err != nil {
-		return err
-	}
-	parts := bytes.SplitN(data, []byte(separator), 2)
+	parts := bytes.SplitN(data, []byte(SepNull), 2)
 	fmt.Print(string(parts[1]))
 	return nil
 }
@@ -89,31 +113,28 @@ func runHashObjCmd(file string) error {
 	if err != nil {
 		return err
 	}
+	header := fmt.Sprintf("%s %d%s", TypeBlob, len(content), SepNull)
 
-	hasher := sha1.New()
-	header := []byte(fmt.Sprintf("blob %d%s", len(content), separator))
-	if _, err := hasher.Write(header); err != nil {
+	var buf bytes.Buffer
+	if _, err := buf.WriteString(header); err != nil {
 		return err
 	}
-	if _, err := hasher.Write(content); err != nil {
+	if _, err := buf.Write(content); err != nil {
 		return err
 	}
-	hash := fmt.Sprintf("%x", hasher.Sum(nil))
 
-	path := filepath.Join(".git", "objects", hash[:2], hash[2:])
+	hash := fmt.Sprintf("%x", sha1.New().Sum(buf.Bytes()))
+
+	path := objectPath(hash)
 	if err := os.Mkdir(filepath.Dir(path), 0750); err != nil && !os.IsExist(err) {
 		return err
 	}
 	object, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
+	if err != nil && !os.IsExist(err) {
 		return err
 	}
-
 	writer := zlib.NewWriter(object)
-	if _, err := writer.Write(header); err != nil {
-		return err
-	}
-	if _, err := writer.Write(content); err != nil {
+	if _, err := writer.Write(buf.Bytes()); err != nil {
 		return err
 	}
 	writer.Close()
@@ -124,27 +145,40 @@ func runHashObjCmd(file string) error {
 }
 
 func runLsTreeCmd(sha string) error {
-	dirName := sha[:2]
-	fileName := sha[2:]
-
-	f, err := os.Open(filepath.Join(".git", "objects", dirName, fileName))
+	data, err := decodeObject(sha)
 	if err != nil {
 		return err
 	}
-
-	r, err := zlib.NewReader(f)
-	if err != nil {
-		return err
-	}
-	data, err := ioutil.ReadAll(r)
-	if err != nil {
-		return err
-	}
-
-	lines := bytes.Split(data, []byte(separator))
+	lines := bytes.Split(data, []byte(SepNull))
 	for _, line := range lines[1 : len(lines)-1] {
 		sep := bytes.Split(line, []byte(" "))
 		fmt.Println(string(sep[len(sep)-1]))
 	}
 	return nil
+}
+
+func decodeObject(sha string) ([]byte, error) {
+	obj, err := os.Open(objectPath(sha))
+	if err != nil {
+		return nil, err
+	}
+	r, err := zlib.NewReader(obj)
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadAll(r)
+}
+
+func objectPath(sha string) string {
+	return filepath.Join(".git", "objects", sha[:2], sha[2:])
+}
+
+func tpeToStr(tpe string) string {
+	switch tpe {
+	case TypeBlob:
+		return "blob"
+	case TypeTree:
+		return "tree"
+	}
+	return "unknown"
 }
